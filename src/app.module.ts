72ee2d0ba0835472025b7588e491e65340e9b20a
= <=>
2024-03-16 09:06:30 +0700

    changes for can deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PersonController } from './person/person.controller';
 import { PersonModule } from './person/person.module';
 
 @Module({
   imports: [AppModule, PersonModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
